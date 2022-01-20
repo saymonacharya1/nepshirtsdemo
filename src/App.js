d84315a455ed2fb3884c0d505d953e42d0937a71
saymonacharya1 <acharyasaymon@gmail.com>
2022-01-20 17:54:20 +0545

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css';
+import React, { useState, useEffect } from "react";
+import Navbar from './Navbar';
+import img1 from "./img/welcome.png";
+import img2 from "./img/mountain1.png";
+import img3 from "./img/mountain2.png";
+
+
+const App = () => {
+
+  const [offset, setOffset] = useState();
+  const handleScroll = () => setOffset(window.pageYOffset);
+  window.addEventListener('scroll', handleScroll);
+
+  return ( 
+  <div className='home'>
+    <Navbar />
+    <section className='A'>
+      
+      <img src={img2} alt="" id='img2'
+      style={{width: (100 + offset * 1) + '%'}} 
+      />
+      <img src={img3} alt="" id='img3' 
+      style={{width: (100 + offset * 0.2) + '%'}} 
+      />
+      <h1 id='text' 
+      style={{top:`-${100 + offset * 0.3 + '%'}`}}>
+        Welcome to NEP-SHIRTS</h1>
+    </section>
+
+    <section className='B'>
+      <h1>T-SHIRTS</h1>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta, officia nobis velit harum unde architecto corrupti qui ad, necessitatibus fuga nisi tenetur cumque explicabo nostrum nihil voluptatibus ab exercitationem quod.
+      Consequatur ratione harum deleniti suscipit eos incidunt officia dolorum accusamus omnis beatae sapiente autem ipsum labore, quis in neque iste laborum itaque quasi? Fugit error, quae magni voluptatem nam ratione.
+      Consequuntur quidem deleniti tempore cumque mollitia dignissimos harum numquam voluptatibus dolorum. Quibusdam nobis dicta voluptate dolorem sunt dignissimos vitae illum non cumque. Quod assumenda, in et necessitatibus recusandae saepe sunt.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, officiis commodi beatae laudantium soluta repudiandae nam aperiam nostrum recusandae tempora ipsum corrupti ex ipsam vero libero? Optio cumque perferendis provident.
+      Saepe, exercitationem? Veritatis culpa nemo accusantium accusamus esse totam, dolorem dicta aliquam dolores quod omnis distinctio architecto sapiente. Cumque optio, odit asperiores omnis vero laboriosam libero velit hic officiis eius?
+      Earum repellat nisi nihil provident quas. Libero nulla, officiis vel illum dolore harum quae error, corrupti similique nisi ex praesentium enim voluptas modi doloribus eveniet animi atque asperiores placeat excepturi.
+      Harum blanditiis reprehenderit distinctio delectus a quo officiis vero quia fugit! Facere dolorem, velit nemo laboriosam sit aperiam nobis at possimus corrupti excepturi aut? Eos, molestias nobis! Doloribus, aspernatur magni!
+      Amet officia laborum aliquid quae distinctio soluta, beatae, aut, dignissimos quam temporibus praesentium unde dolores provident recusandae aspernatur error in atque quo quis. Delectus harum rem labore, reprehenderit nihil quaerat?
+      Quas dignissimos quos quam blanditiis fugiat. Velit, magni tenetur quasi maiores deleniti sint aperiam culpa repudiandae, illo eos quidem harum. Eos omnis hic amet doloremque? Dolorum quos nisi enim reprehenderit.
+      Ab natus reprehenderit officiis veniam aperiam temporibus autem. Nisi velit cumque alias debitis distinctio! Voluptatum ullam aperiam totam impedit maxime. Quia eligendi veritatis neque autem eos ducimus magnam labore ullam.
+      Corporis quo obcaecati delectus recusandae atque. Iste, suscipit consequatur! Aliquid totam ex sequi perferendis recusandae? Dolor, perferendis maiores! Porro quas numquam enim excepturi possimus praesentium laborum, rem perspiciatis qui exercitationem?
+      Officiis, reiciendis quaerat! Autem, delectus vitae? Suscipit sunt magnam pariatur laboriosam inventore aperiam nulla eos nobis ea iste quia repellat ut, nostrum sequi distinctio culpa ipsam non, sint ex quaerat?
+      Iusto quae adipisci dolorum iste in ducimus eos suscipit cum, veniam dolore laudantium illo accusamus incidunt delectus officiis nostrum inventore obcaecati recusandae distinctio commodi sint? Maiores quo tenetur unde voluptatem?
+    </section>
+
+    <section className='C'></section>
+
+    <section className='D'></section>
+    
+
+  </div>
+   );
 }
-
+ 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import './index.css';
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="head1">
+                <h1>
+                    <span>NEP</span>SHIRTS
+                </h1>
+            </div>
+
+            <div className="head2">
+                <div className="links">
+                    <a href="">HOME</a>
+                    <a href="">T-SHIRTS</a>
+                    <a href="">SHIRTS</a>
+                    <a href="">CONTACT US</a>
+                </div>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
